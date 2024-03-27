a7436c3a4952c1e3966fb8020383bde9cbabb6c5
Aparna <aparnasukesh0723@gmail.com>
2024-03-27 10:40:54 +0530

    Q-07 solved

---
+++ Q-07/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 123
+	//Output: 321
+
+	fmt.Println(reverse(x))
+
+}
+func reverse(x int) int {
+	const MaxInt32 = 1<<31 - 1
+	const MinInt32 = -1 << 31
+	sum := 0
+	for x != 0 {
+		rem := x % 10
+		if sum > MaxInt32/10 || (sum == MaxInt32/10 && rem > 7) {
+			return 0
+		}
+		if sum < MinInt32/10 || (sum == MinInt32/10 && rem < -8) {
+			return 0
+		}
+		sum = (sum * 10) + rem
+		x = x / 10
+	}
+	return sum
+}
